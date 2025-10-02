02268f3f3130be857ed03a2ebc811643309a49bc
Rifat Hossain <mdrifathossain472@gmail.com>
2025-10-02 22:54:04 +0600

    03_typeConversion

---
+++ 03_typeConversion.js
@@ -0,0 +1,60 @@
+/*let score = "33aa"
+let score1 = 33.22
+
+console.log(typeof 20.02); //number
+//in java script there is not difference between int or float or double.everything is consider as number 
+console.log(typeof score);
+console.log(typeof (score));
+
+let scoreValue = Number(score)  //this convert a value to number
+let scoreString = String(score1)  // this convert a value to String
+
+console.log(typeof scoreValue); //number
+console.log(scoreValue);  //NaN.meaning of this is "Not a number".it is a type.
+console.log();
+console.log(typeof scoreString); //string
+console.log(scoreString); //33.22
+console.log();
+
+
+
+//"33" toNumber = 33
+//"33af" toNumber = NaN
+//true => 1; false => 0
+//null toNumber = 0
+//undefined toNumber = NaN
+
+let isLoggedIn = 1
+let isLoggedInString = ""
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedInString = Boolean(isLoggedInString)
+
+
+console.log(typeof booleanIsLoggedIn); //boolean
+console.log(booleanIsLoggedIn); //true
+console.log();
+
+console.log(typeof booleanIsLoggedInString); //boolean
+console.log(booleanIsLoggedInString); //false
+
+//1 => true; 0=> false
+// ""=> false
+// "ffad" => true*/
+
+let scoreNumber = 20
+let scoreNumberNull = null
+
+let objectScore = Object(scoreNumber) 
+let objectScoreNull = Object(scoreNumberNull) 
+
+console.log(objectScore); //[number: 20]
+console.log(typeof objectScore); //object
+
+console.log(objectScoreNull); //{}
+
+
+
+
+
