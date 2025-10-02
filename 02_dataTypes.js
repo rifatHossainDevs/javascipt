de67f3b6366851fe09d3f8dca4c273f27a4c5b24
Rifat Hossain <mdrifathossain472@gmail.com>
2025-10-02 21:19:55 +0600

    02_dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; 
+// treat all JS code as newer version.this is not compulsory.because default is newer version.but it is good to write
+
+// alert( 3 + 3) // we are using nodejs, not browser.this is allowed in browser not is nodejs
+
+console.log(3 
+    +
+     3) // code readability should be high.this type of writing is not good.
+console.log("rifatHossain")
+
+
+let name = "rifatHossain"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
