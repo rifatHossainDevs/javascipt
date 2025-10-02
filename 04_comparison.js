2354516ed4bd991cf7100c5fbc41192e4675aaeb
Rifat Hossain <mdrifathossain472@gmail.com>
2025-10-03 01:05:05 +0600

    04_comparison

---
+++ 04_comparison.js
@@ -0,0 +1,31 @@
+//console.log(2>1); //true
+//console.log(3<1); //false
+
+// console.log("2" > 2); //false
+// console.log("02" > 1); //true
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+// console.log(undefined > 0); //false
+// console.log(undefined == 0); //false
+// console.log(undefined >= 0); //false
+
+// "===" => this check strictly.in the above all are convert string to number then check 
+// but "===" not only convert and check but also it check the type and compare
+
+console.log("2" === 2); //false.because there it check type of both and doesn't match.so that it return false
+
+
+
+
+
+
+
+
+
+
+
+
+
